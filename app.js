1c5cbf47aae3244cbd23070ab33665b65b27e6ec
Simen23 <95966636+Simen23@users.noreply.github.com>
2023-02-18 16:21:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function solution() {
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('a').value;
+    document.getElementById('sum').innerText = +a + +b
+}
+function mul() {
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('a').value;
+    document.getElementById('sum').innerText = +a * +b
+}
