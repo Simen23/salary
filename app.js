af0c3ab06a829ace4d2cf1b26a434e3ad2af8be7
Simen23 <95966636+Simen23@users.noreply.github.com>
2023-02-18 16:22:40 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-function solution() {
-    let a = document.getElementById('a').value;
-    let b = document.getElementById('a').value;
-    document.getElementById('sum').innerText = +a + +b
-}
-function mul() {
-    let a = document.getElementById('a').value;
-    let b = document.getElementById('a').value;
-    document.getElementById('sum').innerText = +a * +b
+function salary() {
+    let Hours = document.getElementById('Hours').value
+    let Days = document.getElementById('Days').value
+    let SalaryForHours = document.getElementById('SalaryForHours').value
+
+    document.getElementById('sum').innerText = (+Hours * +Days * +SalaryForHours)
 }
